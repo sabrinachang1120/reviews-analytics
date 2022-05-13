13b994b3adb937ac0cf0d3b5d71a843ec54f92b6
Sabrina Chang <sabrina@SabrinadeMacBook-Air.local>
2022-05-13 18:23:49 +0800

    calculte average words

--- read.py
+++ read.py
@@ -2,9 +2,13 @@ data = []
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-print(len(data))
+print('檔案讀取完了,總共有', len(data),'筆資料')
 
-print(data[0])
-print("-------")
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度是', sum_len/len(data))
+
+
 
